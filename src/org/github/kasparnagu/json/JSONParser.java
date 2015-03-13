d8f5a847c7c696bc8c899a793f39de4bdd32c340
kb <dev@gagu.mooo.com>
2015-03-13 23:35:43 +0100

    Initial commit

---
+++ src/org/github/kasparnagu/json/JSONParser.java
@@ -0,0 +1,102 @@
+package org.github.kasparnagu.json;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class JSONParser {
+	public static class JSONParseException extends Exception {
+		private static final long serialVersionUID = 7078320816065460L;
+
+		public JSONParseException(String cause) {
+			super(cause);
+		}
+	}
+	public static Object parseJSONFile(String file) throws JSONParseException, IOException{
+		return parseJSON(Paths.get(file));
+	}
+	public static Object parseJSON(String text) throws JSONParseException{
+		return parseJSON(new Scanner(text));
+	}
+	public static Object parseJSON(Path file) throws JSONParseException, IOException{
+		return parseJSON(new Scanner(file));
+	}
+	public static Object parseJSON(Scanner s) throws JSONParseException{
+		Object ret = null;
+		skipWhitespace(s);
+		if(s.findWithinHorizon("\\{", 1) != null){
+			HashMap<Object, Object> retMap = new HashMap<>();
+			ret = retMap;
+			skipWhitespace(s);
+			if(s.findWithinHorizon("\\}", 1) == null){
+				while(s.hasNext()){
+					Object key = parseJSON(s);
+					skipWhitespace(s);
+					if(s.findWithinHorizon(":", 1) == null){
+						throw new JSONParseException("Expected :");
+					}
+					Object value = parseJSON(s);
+					retMap.put(key, value);
+					skipWhitespace(s);
+					if(s.findWithinHorizon(",", 1)== null){
+						break;
+					}
+				}			
+				if(s.findWithinHorizon("\\}", 1) == null){
+					throw  new JSONParseException("Expected }");
+				}
+			}
+		}else if(s.findWithinHorizon("\"", 1) != null){
+			ret = s.findWithinHorizon("(\\\\\\\\|\\\\\"|[^\"])*",0);
+			if(s.findWithinHorizon("\"", 1) == null){
+				throw  new JSONParseException("Expected quote");				
+			}
+		}else if(s.findWithinHorizon("'", 1) != null){
+			ret = s.findWithinHorizon("(\\\\\\\\|\\\\'|[^'])*",0);
+			if(s.findWithinHorizon("'", 1) == null){
+				throw  new JSONParseException("Ex??ected quote");				
+			}		
+		}else if(s.findWithinHorizon("\\[", 1) != null){
+			ArrayList<Object> retList = new ArrayList<>();
+			ret = retList;
+			skipWhitespace(s);
+			if(s.findWithinHorizon("\\]", 1) == null){
+				while(s.hasNext()){
+					retList.add(parseJSON(s));
+					skipWhitespace(s);
+					if(s.findWithinHorizon(",", 1)== null){
+						break;
+					}
+				}
+				if(s.findWithinHorizon("\\]", 1) == null){
+					throw  new JSONParseException("Expected , or ]");
+				}
+			}
+		}else if(s.findWithinHorizon("true",4) != null){
+			ret = true;
+		}else if(s.findWithinHorizon("false",5) != null){
+			ret = false;
+		}else if(s.findWithinHorizon("null",4) != null){
+			ret = null;
+		}else{
+			String numberStart = s.findWithinHorizon("[-0-9+eE]", 1);
+			if(numberStart != null){
+				String numStr = numberStart + s.findWithinHorizon("[-0-9+eE.]*", 0);
+				if(numStr.contains(".") | numStr.contains("e")){
+					ret = Double.valueOf(numStr);
+				}else{
+					ret = Long.valueOf(numStr);
+				}
+			}else{
+				throw new JSONParseException("No JSON value found");
+			}
+		}
+		return ret;
+	}
+	private static void skipWhitespace(Scanner s) {
+		s.findWithinHorizon("\\s*", 0);
+	}
+}
---
+++ src/org/github/kasparnagu/json/JSONSerializer.java
@@ -0,0 +1,72 @@
+package org.github.kasparnagu.json;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+import java.util.Map;
+
+public class JSONSerializer {
+	public static class JSONSerializerException extends Exception {
+		private static final long serialVersionUID = -4942348678107203496L;
+
+		public JSONSerializerException(String cause) {
+			super(cause);
+		}
+	}
+	public static String serializeJSON(Object o) throws JSONSerializerException{
+		StringWriter wrt = new StringWriter();
+		try {
+			serializeJSON(o,wrt);
+		} catch (IOException e) {
+			throw new JSONSerializerException("IOException during serialization");
+		}
+		return wrt.toString();
+	}
+	public static void serializeJSON(Object o, Writer w) throws JSONSerializerException, IOException {
+		if(o == null){
+			w.write("null");
+		}else if(o instanceof String){
+			w.write("\"");
+			w.write((String)o);
+			w.write("\"");			
+		}else if(o instanceof Integer){
+			w.write(Integer.toString((Integer)o));
+		}else if(o instanceof Long){
+			w.write(Long.toString((Long)o));
+		}else if(o instanceof Double){
+			w.write(Double.toString((Double)o));
+		}else if(o instanceof Boolean){
+			w.write(Boolean.toString((Boolean)o));
+		}else if(o instanceof List){
+			boolean first = true;
+			w.write("[");
+			for(Object l:(List<?>)o){
+				if(first){
+					first = false;
+				}else{
+					w.write(",");
+				}
+				serializeJSON(l, w);
+			}
+			w.write("]");
+		}else if(o instanceof Map){
+			boolean first = true;
+			w.write("{");
+			for(Map.Entry<?,?> l:((Map<?,?>)o).entrySet()){
+				if(first){
+					first = false;
+				}else{
+					w.write(",");
+				}
+				serializeJSON(l.getKey(), w);
+				w.write(":");
+				serializeJSON(l.getValue(), w);
+			}
+			w.write("}");
+		}else{
+			throw new JSONSerializerException("Can not serialize type:"+o.getClass().getSimpleName());
+		}
+	}
+	
+}
